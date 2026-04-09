352ddd1a013b1972d02c67874bbf462204a0a861
mrpawar221@gmail.com <nalwadesumati@gmail.com>
2026-04-09 12:23:01 +0530

    use mat card

--- src/app/component/posts/post-card/post-card.ts
+++ src/app/component/posts/post-card/post-card.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Ipost } from '../../../models/posts';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-post-card',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './post-card.html',
   styleUrl: './post-card.scss',
 })
